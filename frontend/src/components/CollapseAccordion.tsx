82993b27e242591c3f90bbb05904d4f9dd68d732
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-29 21:37:18 +0300

    change css style card

--- frontend/src/components/CollapseAccordion.tsx
+++ frontend/src/components/CollapseAccordion.tsx
@@ -2,10 +2,11 @@ import {Collapse, Box, Accordion } from '@mantine/core';
 import {Appointment} from '../store/patientsSlice';
 import React from "react";
 import {transformDate} from '../utils';
+import card from '../assets/scss/card.module.scss'
 
 interface CollapseProps {
-  isOpen: boolean,
-  appointments: Appointment[]
+  isOpen: boolean;
+  appointments: Appointment[];
 }
 
 function CollapseAccordion(props: CollapseProps) {
@@ -14,7 +15,7 @@ function CollapseAccordion(props: CollapseProps) {
   return (
     <Box maw={400} mx="auto">
       <Collapse in={isOpen}>
-        <Accordion>
+        <Accordion className={card.accordionBlock}>
           {appointments.map((appointment) => (
             <Accordion.Item key={appointment.id} value={String(appointment.id)}>
               <Accordion.Control>{transformDate(appointment.date_start, true)} {appointment.status}</Accordion.Control>
