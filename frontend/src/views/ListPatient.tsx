08207bfe7109bc233d8a28c0c7c6365ef141ff13
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-09-29 17:48:56 +0300

    create simple logout

--- frontend/src/views/ListPatient.tsx
+++ frontend/src/views/ListPatient.tsx
@@ -4,9 +4,13 @@ import CardItem from '../components/CardItem';
 import patient from '../assets/scss/listPatients.module.scss';
 import {fetchPatients} from "../store/patientsSlice";
 import { RootState, AppDispatch } from '../store';
+import { Button } from '@mantine/core';
+import {Navigate, useNavigate} from "react-router-dom";
 
 function ListPatient() {
   const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { patients, loading, error } = useSelector((state: RootState) => state.patients);
 
   useEffect(() => {
@@ -16,11 +20,21 @@ function ListPatient() {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
+  function handleLogout() {
+    localStorage.removeItem('loginTime');
+    navigate('/')
+  }
+
   return (
     <div className={patient.page}>
-      {patients.map((patient) => (
+      <div>
+        <Button variant="filled" onClick={handleLogout}>Выход</Button>
+      </div>
+      <div>
+        {patients.map((patient) => (
         <CardItem key={patient.id} mainCss='patient' data={patient}/>
         ))}
+      </div>
     </div>
   )
 }
