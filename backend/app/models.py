3f7ff7f18329e99e274aee1e8bbbffab6ab0d09c
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-10-02 21:53:50 +0300

    Column to mapped_column & DeclarativeBase

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import (Column,
-                        Integer,
+from sqlalchemy import (Integer,
                         String,
                         TIMESTAMP,
                         DATE,
@@ -7,12 +6,15 @@ from sqlalchemy import (Column,
                         Enum,
                         JSON,
                         Boolean)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import (relationship,
+                            mapped_column,
+                            DeclarativeBase)
 from .constants import (statuses,
-                           genders)
+                        genders)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+  pass
 
 
 class Resources(Base):
@@ -25,10 +27,10 @@ class Resources(Base):
   """
   __tablename__ = 'resources'
 
-  id = Column(Integer, primary_key=True, index=True)
-  type = Column(String, nullable=False)
-  last_update = Column(TIMESTAMP(timezone=True))
-  offline = Column(Boolean, nullable=False, default=False)
+  id = mapped_column(Integer, primary_key=True, index=True)
+  type = mapped_column(String, nullable=False)
+  last_update = mapped_column(TIMESTAMP(timezone=True))
+  offline = mapped_column(Boolean, nullable=False, default=False)
 
 
 class Appointments(Base):
@@ -47,16 +49,16 @@ class Appointments(Base):
   """
   __tablename__ = 'appointments'
 
-  id = Column(Integer, primary_key=True, index=True)
-  status = Column(Enum(*statuses.keys(), name='status_enum'), nullable=False)
-  service_details = Column(JSON)  # serviceCategory, serviceType, specialty
-  date_start = Column(TIMESTAMP(timezone=True))
-  date_end = Column(TIMESTAMP(timezone=True))
-  description = Column(String)
-  participants = Column(JSON)
-  priority = Column(Integer)
-  resource_id = Column(Integer, ForeignKey('resources.id'))
-  patient_id = Column(String, ForeignKey('patients.patient_id'))
+  id = mapped_column(Integer, primary_key=True, index=True)
+  status = mapped_column(Enum(*statuses.keys(), name='status_enum'), nullable=False)
+  service_details = mapped_column(JSON)  # serviceCategory, serviceType, specialty
+  date_start = mapped_column(TIMESTAMP(timezone=True))
+  date_end = mapped_column(TIMESTAMP(timezone=True))
+  description = mapped_column(String)
+  participants = mapped_column(JSON)
+  priority = mapped_column(Integer)
+  resource_id = mapped_column(Integer, ForeignKey('resources.id'))
+  patient_id = mapped_column(String, ForeignKey('patients.patient_id'))
 
   resource = relationship('Resources')
   patient = relationship('Patients', back_populates='appointments')
@@ -75,12 +77,12 @@ class Patients(Base):
   """
   __tablename__ = 'patients'
 
-  id = Column(Integer, primary_key=True, index=True)
-  patient_id = Column(String, unique=True, nullable=False)
-  identifier = Column(String)
-  fullname = Column(String, nullable=False)
-  gender = Column(Enum(*genders.keys(), name='gender_name', nullable=False))
-  birth_date = Column(DATE, nullable=False)
-  address = Column(JSON)
+  id = mapped_column(Integer, primary_key=True, index=True)
+  patient_id = mapped_column(String, unique=True, nullable=False)
+  identifier = mapped_column(String)
+  fullname = mapped_column(String, nullable=False)
+  gender = mapped_column(Enum(*genders.keys(), name='gender_name', nullable=False))
+  birth_date = mapped_column(DATE, nullable=False)
+  address = mapped_column(JSON)
 
   appointments = relationship('Appointments', back_populates='patient')
